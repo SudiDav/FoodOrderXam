07ec1a4020a49663660873340cd5884f3f119778
Sudi Dav <sudisimbadav@gmail.com>
2020-08-18 13:32:03 +0200

    Completed Category view

---
+++ FoodOrderXam/FoodOrderXam/Views/CategoryView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FoodOrderXam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodOrderXam.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoryView : ContentPage
+    {
+        private Category _category;
+
+        public CategoryView()
+        {
+            InitializeComponent();
+        }
+
+        public CategoryView(Category category)
+        {
+            _category = category;
+        }
+
+        private void ImageButton_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
